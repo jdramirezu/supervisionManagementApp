121f0477dcd8d267e9644ee0af8470b1d246b781
Jose David Ramirez <117590011+jdramirezu@users.noreply.github.com>
2025-03-05 04:49:10 -0500

    Adjustments to connect to server

--- src/contexts/EmployeeContext.jsx
+++ src/contexts/EmployeeContext.jsx
@@ -5,15 +5,30 @@ const EmployeeContext = createContext();
 export const EmployeeProvider = ({ children }) => {
     const [employees, setEmployees] = useState([]);
     const [selectedEmployee, setSelectedEmployee] = useState(null);
-    const [route, setRoute] = useState("login");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [newCandidateData, setNewCandidateData] = useState({
+        id: '15',
+        fullName: "",
+        preferredName: "",
+        email: "",
+        employeeID: "",
+        phoneNumber: "",
+        status: "",
+        area: "",
+        contractType: "",
+        stage: "",
+        availability: "",
+        comments: ""
+    })
 
     useEffect (() =>{
-        fetch("/data.json")
+        fetch("http://localhost:3000/profiles")
         .then(resp => resp.json())
         .then(info => {
           setEmployees(info);
         })
-        .catch(err => console.log("Error fetching employees: ", err));
+        .catch(err => console.log("Error fetching employees in front: ", err));
     },[]);
 
     const updateEmployee = (updatedEmpInfo) => {
@@ -25,16 +40,70 @@ export const EmployeeProvider = ({ children }) => {
         setSelectedEmployee(updatedEmpInfo);
     }
 
-    const onRouteChange = (newRoute) =>{
-        setRoute(newRoute);
-    }
-
     const onEmployeeClick = employee => {
         setSelectedEmployee(employee);
     }
 
+    const onEmailChange = event =>{
+        setLoginEmail(event.target.value);
+    }
+
+    const onPasswordChange = event =>{
+        setLoginPassword(event.target.value);
+    }
+
+    const onLoginSubmit = (navigate) =>{
+        fetch("http://localhost:3000/login",{
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: loginEmail,
+                password: loginPassword
+            })
+        })
+        .then(resp => resp.json())
+        .then(data =>{
+            if(data === "success"){
+                navigate("/employees");
+            } 
+        })
+    }
+
+    const onCandidateSave = (navigate, newCandidateData) =>{
+        fetch("http://localhost:3000/newCandidate",{
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newCandidateData)
+        })
+        .then(resp => resp.json())
+        .then(data =>{
+            console.log(data);
+            navigate("/employees");
+        
+        })
+    }
+
+    const onDataChange = (event) =>{
+        setNewCandidateData({
+            ...newCandidateData, [event.target.name]:event.target.value
+        });
+    }
+
     return (
-        <EmployeeContext.Provider value={{selectedEmployee, onEmployeeClick, route, onRouteChange, employees, setSelectedEmployee, updateEmployee}}>
+        <EmployeeContext.Provider value={
+            {
+                selectedEmployee,
+                onEmployeeClick,
+                employees,
+                setSelectedEmployee,
+                updateEmployee,
+                onEmailChange,
+                onPasswordChange,
+                onLoginSubmit,
+                onCandidateSave,
+                onDataChange,
+                newCandidateData
+            }}>
             {children}
         </EmployeeContext.Provider>
     );
